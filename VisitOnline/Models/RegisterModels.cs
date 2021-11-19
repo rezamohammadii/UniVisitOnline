70bc135c4d46c74d0924fe0b564373cf657d9ba4
reza mohammadi <ps10mrm.rm@gmail.com>
2021-11-19 23:38:44 +0330

    add gitignore

---
+++ VisitOnline/Models/RegisterModels.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VisitOnline.Models
+{
+    public class RegisterModels
+    {
+        [Display(Name = "شماره موبایل")]
+        [Required(ErrorMessage = "نباید بدون مقدار باشد")]
+        [MaxLength(11, ErrorMessage = "مقدار {0} نباید بیش تر از {1} کاراکتر باشد")]
+        [MinLength(11, ErrorMessage = "مقدار {0} نباید کم تر از {1} کاراکتر باشد")]
+        [Phone(ErrorMessage = "فقط عدد می توانید وارد کنید")]
+        public string Mobile { get; set; }
+
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "نباید بدون مقدار باشد")]
+        [MaxLength(100, ErrorMessage = "مقدار {0} نباید بیش تر از {1} کاراکتر باشد")]
+        [MinLength(4, ErrorMessage = "مقدار {0} نباید کم تر از {1} کاراکتر باشد")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "تایید کلمه عبور")]
+        [Required(ErrorMessage = "نباید بدون مقدار باشد")]
+        [MaxLength(100, ErrorMessage = "مقدار {0} نباید بیش تر از {1} کاراکتر باشد")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "کلمه های عبور همخوانی ندارند")]
+        public string ConfirmPassword { get; set; }
+        [Display(Name = "نام و نام خانوادگی")]
+        [Required(ErrorMessage = "نباید بدون مقدار باشد")]
+        [MaxLength(11, ErrorMessage = "مقدار {0} نباید بیش تر از {1} کاراکتر باشد")]
+        [MinLength(11, ErrorMessage = "مقدار {0} نباید کم تر از {1} کاراکتر باشد")]
+        [Phone(ErrorMessage = "فقط عدد می توانید وارد کنید")]
+        public string NameFamily { get; set; }
+        public bool IsDoctorOrNo { get; set; }
+
+    }
+}
